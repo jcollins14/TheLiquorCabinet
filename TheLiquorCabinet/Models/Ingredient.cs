f40066c373a01357f2cf23106dc4fd4875d3c628
Jake Collins <jakecollins1014@gmail.com>
2020-06-03 21:53:50 -0400

    added models

---
+++ TheLiquorCabinet/Models/Ingredient.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TheLiquorCabinet.Models
+{
+    public class Ingredient
+    {
+        [JsonProperty("idIngredient")]
+        public int ID { get; set; }
+        [JsonProperty("strIngredient")]
+        public string Name { get; set; }
+        [JsonProperty("strDescription")]
+        public string Description { get; set; }
+        [JsonProperty("strType")]
+        public string Type { get; set; }
+        public bool IsAlcoholic { get; set; }
+        [JsonProperty("strABV")]
+        public int ABV { get; set; }
+
+        public Ingredient(string APItext)
+        {
+            JObject parse = JObject.Parse(APItext);
+            this.ID = parse.Property("idIngredient").ToObject<int>();
+            this.Name = parse.Property("strIngredient").ToObject<string>();
+            this.Description = parse.Property("strDescription").ToObject<string>();
+            this.Type = parse.Property("strType").ToObject<string>();
+            if (parse.Property("strAlcohol").ToObject<string>() == "Yes")
+            {
+                this.IsAlcoholic = true;
+            }
+            else
+            {
+                this.IsAlcoholic = false;
+            }
+            this.ABV = parse.Property("strABV").ToObject<int>();
+        }
+    }
+}
---
+++ TheLiquorCabinet/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLiquorCabinet.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public List<int> Favorites { get; set; }
+        public List<Ingredient> Cabinet { get; set; }
+        public User(string username)
+        {
+            this.Username = username;
+            this.Favorites = new List<int>();
+            this.Cabinet = new List<Ingredient>();
+        }
+    }
+}
