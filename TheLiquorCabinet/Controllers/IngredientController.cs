4cf782bdcfde64452ec7368f200bd11529c306d2
Andrew Waltman <Andrew.W.Waltman@gmail.com>
2020-06-04 20:52:18 -0400

    Trying to get things preparred - no merge necessary

--- TheLiquorCabinet/Controllers/IngredientController.cs
+++ TheLiquorCabinet/Controllers/IngredientController.cs
@@ -13,18 +13,34 @@ namespace TheLiquorCabinet.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            await GetIngredient("1");
+            await GetIngredient("9973533");
             return View();
         }
 
-        public async Task<IActionResult> GetIngredient(string drinkId)
+        public async Task<IActionResult> GetIngredient(string ingredientId)
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/");
+            client.BaseAddress = new Uri("https://www.themealdb.com/api/json/v2/9973533/latest.php");
             //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; GrandCircus/1.0)");
-            var response = await client.GetStringAsync("1/lookup.php?iid=" + drinkId);
+            var response = await client.GetStringAsync("9973533/list.php?i=list" + ingredientId);
             Ingredient result = new Ingredient(response);
             return View(result);
+        }
+
+        public async Task<IActionResult> GetAllIngredients()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.themealdb.com/api/json/v2/9973533/latest.php");
+            //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; GrandCircus/1.0)");
+            var response = await client.GetStringAsync("9973533/list.php?i=list");
+            IngredientList result = new IngredientList(response);
+            return RedirectToAction("SearchByIngredient");
+        }
+
+        public IActionResult SearchByIngredient()
+        {
+
+            return View();
         }
     }
 }
