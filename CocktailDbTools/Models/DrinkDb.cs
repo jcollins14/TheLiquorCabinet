c56598d4f8887ad0f9724d5923305eb6e10e48bf
Austin <austingpowel@gmail.com>
2020-06-08 19:38:39 -0400

    Database coling progress?

--- CocktailDbTools/Models/DrinkDb.cs
+++ CocktailDbTools/Models/DrinkDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,21 +7,40 @@ using System.Threading.Tasks;
 
 namespace CocktailDbTools.Models
 {
-
+    public class DrinkResponse
+    {
+        public List<DrinkDb> Drinks { get; set; }
+        public DrinkResponse(string ApiText)
+        {
+            JObject parse = JObject.Parse(ApiText);
+            foreach (var item in parse["drinks"])
+            {
+                this.Drinks.Add(new DrinkDb { idDrink = (string)item["idDrink"], 
+                    strDrink = (string)item["strDrink"],
+                    strIngredient1 = (string)item["strIngredient1"], 
+                    strIngredient2 = (string)item["strIngredient2"],
+                    strIngredient3 = (string)item["strIngredient3"], 
+                    strIngredient4 = (string)item["strIngredient4"], 
+                    strIngredient5 = (string)item["strIngredient5"],
+                    strIngredient6 = (string)item["strIngredient6"],
+                    strIngredient7 = (string)item["strIngredient7"],
+                    strIngredient8 = (string)item["strIngredient8"],
+                    strIngredient9 = (string)item["strIngredient9"],
+                    strIngredient10 = (string)item["strIngredient10"],
+                    strIngredient11 = (string)item["strIngredient11"],
+                    strIngredient12 = (string)item["strIngredient12"],
+                    strIngredient13 = (string)item["strIngredient13"],
+                    strIngredient14 = (string)item["strIngredient14"],
+                    strIngredient15 = (string)item["strIngredient15"]
+                });
+            }
+        }
+    }
     public class DrinkDb
     {
         [Key]
         public string idDrink { get; set; }
         public string strDrink { get; set; }
-        public string strDrinkAlternate { get; set; }
-        public string strTags { get; set; }
-        public string strVideo { get; set; }
-        public string strCategory { get; set; }
-        public string strIBA { get; set; }
-        public string strAlcoholic { get; set; }
-        public string strGlass { get; set; }
-        public string strInstructions { get; set; }
-        public string strDrinkThumb { get; set; }
         public string strIngredient1 { get; set; }
         public string strIngredient2 { get; set; }
         public string strIngredient3 { get; set; }
@@ -36,22 +56,7 @@ namespace CocktailDbTools.Models
         public string strIngredient13 { get; set; }
         public string strIngredient14 { get; set; }
         public string strIngredient15 { get; set; }
-        public string strMeasure1 { get; set; }
-        public string strMeasure2 { get; set; }
-        public string strMeasure3 { get; set; }
-        public string strMeasure4 { get; set; }
-        public string strMeasure5 { get; set; }
-        public string strMeasure6 { get; set; }
-        public string strMeasure7 { get; set; }
-        public string strMeasure8 { get; set; }
-        public string strMeasure9 { get; set; }
-        public string strMeasure10 { get; set; }
-        public string strMeasure11 { get; set; }
-        public string strMeasure12 { get; set; }
-        public string strMeasure13 { get; set; }
-        public string strMeasure14 { get; set; }
-        public string strMeasure15 { get; set; }
-        public string dateModified { get; set; }
+
     }
 
 }
