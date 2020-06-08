642adab47b7f0b54d2ec235dfec7e5ed166e1ad2
Dylan Rule <dylanmrule@yahoo.com>
2020-06-08 19:50:31 -0400

    Picture Displayed on Home Page

--- TheLiquorCabinet/Controllers/HomeController.cs
+++ TheLiquorCabinet/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,8 +12,11 @@ namespace TheLiquorCabinet.Controllers
 {
     public class HomeController : Controller
     {
+        private HttpClient _client;
+        public string ApiKey = "api/json/v2/9973533";
         private readonly ILogger<HomeController> _logger;
 
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -23,6 +27,21 @@ namespace TheLiquorCabinet.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Home()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v2/");
+            //client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; GrandCircus/1.0)");
+            var response = await client.GetStringAsync("1/random.php");
+            Drink result = new Drink(response);
+            
+            return View(result);
+        }
+
+        public IActionResult HomeNA()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
