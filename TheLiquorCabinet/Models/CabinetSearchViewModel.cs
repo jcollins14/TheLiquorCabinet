f460319a47a9dede4272e59b3674bb48afa03419
apowel <austingpowel@gmail.com>
2020-06-18 19:56:41 -0400

    Cabinet Search now returns results for drinks only missing one ingredient!

---
+++ TheLiquorCabinet/Models/CabinetSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLiquorCabinet.Models
+{
+    public class CabinetSearchViewModel
+    {
+        public List<Drink> CanMake { get; set; }
+        public List<Drink> MissingOne { get; set; }
+        public CabinetSearchViewModel()
+        {
+            CanMake = new List<Drink>();
+            MissingOne = new List<Drink>();
+        }
+    }
+}
