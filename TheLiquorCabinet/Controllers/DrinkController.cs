44e3ecef3ea1e3375d5b45345c3e6014d51114bd
Dylan Rule <dylanmrule@yahoo.com>
2020-06-22 21:59:34 -0400

    Updates to Drink List Views

--- TheLiquorCabinet/Controllers/DrinkController.cs
+++ TheLiquorCabinet/Controllers/DrinkController.cs
@@ -125,7 +125,8 @@ namespace TheLiquorCabinet.Controllers
                  string drinkName = (string)parse["drinks"][i]["strDrink"];
                 result.Add(drinkName);
             }
-            ViewBag.IngredientNames = ingredients;
+            string joined = String.Join(", ", ingredients);
+            ViewBag.IngredientNames = joined;
             List<Drink> drinks = await GetDrinks(result);
             return DrinkListView(drinks);
         }
